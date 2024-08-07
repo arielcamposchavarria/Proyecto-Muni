cd0c502e0ed1efc4aeb55352677e60f548bfe71b
arielcamposchavarria <campos2004.05@gmail.com>
2024-08-07 17:07:25 -0600

    Vistas iniciales

---
+++ MuniFrontEnd/src/PaginaInicial/Login.tsx
@@ -0,0 +1,82 @@
+import{ useState } from 'react';
+import './Login.css';
+import {Link, useNavigate} from 'react-router-dom'
+
+function Login() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    id: '',
+    phone: '',
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    // Aquí puedes manejar la lógica para enviar los datos al servidor
+    navigate('/Home')
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="id">ID:</label>
+          <input
+            type="text"
+            id="id"
+            name="id"
+            value={formData.id}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Phone Number:</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="submit-btn">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
